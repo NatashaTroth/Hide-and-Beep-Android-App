58cb0de03d4a966e6f99a08d2e728c2fbc296f23
Natasha Troth <natashatroth@Natashas-MacBook-Pro.local>
2019-03-31 21:35:18 +0200

    feat: add onclick eventlisteners to my buttons and bug fixes

--- app/src/main/java/com/example/tranguyen/gameappproject/LiveRankingActivity.java
+++ app/src/main/java/com/example/tranguyen/gameappproject/LiveRankingActivity.java
@@ -1,7 +1,10 @@
 package com.example.tranguyen.gameappproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LiveRankingActivity extends AppCompatActivity {
 
@@ -11,5 +14,15 @@ public class LiveRankingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_ranking);
+
+        //Click on "Back to game" Button
+        Button liveRankingBackBtn = findViewById(R.id.liveRankingBackBtn);
+        liveRankingBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LiveRankingActivity.this, MainGameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
